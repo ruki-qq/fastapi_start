ae4fcf5f649fffe94ed2e0f306f4ccd68dac0eb5
ruki-qq <ruki@duck.com>
2024-04-16 17:57:58 +0300

    Add count column to order-products assoc tb

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -5,6 +5,8 @@ __all__ = (
     "Post",
     "Product",
     "Profile",
+    "Order",
+    "OrderProductAssociation",
     "User",
 )
 
@@ -13,4 +15,6 @@ from .db_helper import DBHelper, db_helper
 from .post import Post
 from .product import Product
 from .profile import Profile
+from .order import Order
+from .order_product_association import OrderProductAssociation
 from .user import User
